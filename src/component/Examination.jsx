ad8262821a1e44178ce5075132eb8b3b5aaeb101
Madysan Ya <madysanya@gmail.com>
2023-05-12 14:22:42 +0700

    Get All exam

--- src/component/Examination.jsx
+++ src/component/Examination.jsx
@@ -1,24 +1,51 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react'
+import { base_url } from "../BaseUrl";
+import axios from 'axios'
 import Menu from './Menu';
 import ExamLogo from '../image/exam.png';
+import Loading from './Loading';
 
 function Examination() {
+    const [data, setData] = useState([]);
+
+    useEffect(() => {
+        async function allExamination() {
+            await axios.get(base_url + "test")
+                .then(response => {
+                    setData(response.data.data);
+                })
+                .catch(error => {
+                    console.log(error);
+                });
+        }
+        allExamination();
+    }, []);
+
     return (
         <>
             <Menu />
             <div className="max-w-screen-lg mx-auto">
-                <div class="grid grid-cols-4 gap-4">
-                    <div className="rounded overflow-hidden shadow-lg mt-4 p-6 bg-white">
-                        <div className="text-center">
-                            <div className="">
-                                <img className="fill-current m-auto" width="60" src={ExamLogo} alt="" />
+                <div className="grid grid-cols-4 gap-4">
+                    {data.length >= 1 ? (
+                        data.map((exam, index) => (
+                            <div className="rounded overflow-hidden shadow-lg mt-4 p-6 bg-white" key={exam.id}>
+                                <div className="text-center">
+                                    <div className="">
+                                        <img className="fill-current m-auto" width="60" src={ExamLogo} alt="" />
+                                    </div>
+                                    <div className="mt-4">
+                                        <p className="text-base uppercase">{exam.name}</p>
+                                        <p className="text-gray-400">{exam.date}</p>
+                                        <p className="text-gray-400 capitalize ">{exam.duration}</p>
+                                    </div>
+                                </div>
                             </div>
-                            <div className="mt-4">
-                                <p className="text-lg">ANNUAL JOB TEST 2023</p>
-                                <p className="text-gray-400">06 June 2023 | 01:30 Hour</p>
-                            </div>
+                        ))
+                    ) : (
+                        <div className="rounded overflow-hidden shadow-lg mt-4 p-6 bg-white">
+                            <Loading />
                         </div>
-                    </div>
+                    )}
                 </div>
             </div>
         </>
