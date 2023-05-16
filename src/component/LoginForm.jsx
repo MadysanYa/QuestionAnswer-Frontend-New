8e3d40ff7f6ebbb6d9318ba6b4d5b64cb8d912bf
Madysan Ya <madysanya@gmail.com>
2023-05-16 09:17:13 +0700

    login form

--- src/component/LoginForm.jsx
+++ src/component/LoginForm.jsx
@@ -7,11 +7,18 @@ import '../css/App.css';
 function LoginForm() {
     const [username, setUserName] = useState('');
     const [password, setPassword] = useState('');
+    const [error403, setError403] = useState(false);
+    const [errorUsername, setErrorUsername] = useState(false);
+    const [errorPassword, setErrorPassword] = useState(false);
+
     const navigate = useNavigate();
 
     const handleSubmit = async (event) => {
         event.preventDefault();
 
+        setErrorUsername(username ? false : true);
+        setErrorPassword(password ? false : true);
+        
         await axios.post(base_url + "login", {
             "username": username,
             "password": password
@@ -25,13 +32,15 @@ function LoginForm() {
             
         }).catch((error) => {
             console.log(error);
+            setError403((error.response.status === 401) ?? false);
         });
     }
 
     return (
         <div className="h-screen bg-gray-100 flex justify-center items-center">
-            <div className="p-6 h-80 bg-white rounded shadow-xl w-96 align-middle">
-                <form onClick={handleSubmit}>
+            <div className="p-6 h-80 bg-white rounded shadow-xl w-96 align-middle high-auto">
+                <form>
+                    { error403 ? <p className="text-red-600">These credentials do not match our records.</p> : ''}
                     <div className="mb-6">
                         <label className="block text-gray-800 font-bold">Username:</label>
                         <input 
@@ -43,6 +52,7 @@ function LoginForm() {
                             required
                             onChange={e => setUserName(e.target.value)}
                         />
+                        { errorUsername ? <p className="text-red-600 mt-1">The username field is required.</p> : ''}
                     </div>
                     <div className="mb-6">
                         <label className="block text-gray-800 font-bold">Password:</label>
@@ -50,13 +60,14 @@ function LoginForm() {
                             type="password" 
                             name="password" 
                             id="password" 
-                            placeholder="password" 
+                            placeholder="Password" 
                             className="w-full border border-gray-300 py-2 pl-3 rounded mt-2 outline-none focus:ring-indigo-600 :ring-indigo-600" 
                             required
                             onChange={e => setPassword(e.target.value)}
                         />
+                        {errorPassword ? <p className="text-red-600 mt-1">The password field is required.</p> : ''}
                     </div>
-                    <button type="submit" className="cursor-pointer py-2 px-4 block mt-6 text-white font-bold w-full text-center rounded bg-shinhan">Login</button>
+                    <button type="submit" onClick={handleSubmit} className="cursor-pointer py-2 px-4 block mt-6 text-white font-bold w-full text-center rounded bg-shinhan">Login</button>
                 </form>
                 <p className="mt-4 text-center text-sm text-gray-500 font-semibold leading-6">
                     <span className="text-red-600">NOTE: </span>Email & Password are the HR System.
