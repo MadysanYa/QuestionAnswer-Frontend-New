9967b53d9fe34aa18db8c842dc317d927a483578
Madysan Ya <madysanya@gmail.com>
2023-05-18 09:41:25 +0700

    Change base url

--- src/BaseUrl.js
+++ src/BaseUrl.js
@@ -1,1 +1,2 @@
-export const base_url = "http://127.0.0.1:8000/api/";
+export const base_url = "http://10.80.230.161/hr_jobtest/public/api/";
+// export const base_url = "http://127.0.0.1:8000/api/";
